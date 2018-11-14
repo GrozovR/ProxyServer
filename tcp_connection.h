b0d9af1d75877f093735dc94fca2065bdff556fd
Roman Grozov <rmangrzv@gmail.com>
2018-11-15 00:17:11 +0300

    5

--- tcp_connection.h
+++ tcp_connection.h
@@ -7,10 +7,9 @@
 
 std::string increaseIP(const std::string& ip){
     auto resIP = ip;
-
-    //TODO: проверять с какого ip подключается клиент, макс ~251
-    int lastNum = atoi(resIP.substr(resIP.find_last_of('.')));
-
+    //TODO: check max ip number <=252 ?
+    std::string sbstr= resIP.substr(resIP.find_last_of('.')+1,resIP.size() - 1);
+    int lastNum = atoi(sbstr.c_str());
     lastNum += 2;
 
     return resIP;
@@ -22,9 +21,9 @@ class tcp_connection
 public:
     typedef boost::shared_ptr<tcp_connection> pointer;
 
-    static pointer create(boost::asio::io_context& io_context)
+    static pointer create(boost::asio::io_context& io_context1, boost::asio::io_context& io_context2)
     {
-        return pointer(new tcp_connection(io_context));
+        return pointer(new tcp_connection(io_context1, io_context2));
     }
 
     boost::asio::ip::tcp::socket& socket()
@@ -35,15 +34,16 @@ public:
     void start();
 
 private:
-    tcp_connection(boost::asio::io_context& io_context)
-        : m_incomeSocket(io_context)
+    tcp_connection(boost::asio::io_context& io_context1, boost::asio::io_context& io_context2)
+        : m_incomeSocket(io_context1),
+          m_outputSocket(io_context2)
     {}
 
     void createOutPutConnection(boost::asio::io_context& io_context){
         auto incomeAddress = m_incomeSocket.remote_endpoint().address();
         if(incomeAddress.is_v4()){
             std::string addrStr = incomeAddress.to_string();
-            addrStr.
+            auto addressToConnect = increaseIP(addrStr);
         }
 
        // m_outputSocket.async_connect()
